f631d2055474515ef999fc4b285c582dd62995f7
Lui Kelekolio <57427805+lui-kelekolio@users.noreply.github.com>
2020-08-19 20:47:45 +1200

    Update Home.jsx

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -25,10 +25,9 @@ class Home extends React.Component {
 
 
                     <div className="blurb-container">
-                        <div className="blurb-headline">Who are we? </div>
+                        <div className="blurb-headline"></div>
                         <br />
-                        <div className="blurb-text">We are a group of web development students who all happen to love dogs!
-                        We are working together to improve our development skills by creating a platform for dog lovers that can't own dogs and dog owners that don't always have the time to walk them.
+                        <div className="blurb-text">
                         </div>
                     </div>
 

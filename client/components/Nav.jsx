8815f402384b5d5cd265e27a73406cd96159a133
Lui Kelekolio <57427805+lui-kelekolio@users.noreply.github.com>
2020-08-19 23:58:10 +1200

    Update Nav.jsx

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -16,18 +16,21 @@ class Nav extends React.Component {
 
 
         <div className="nav">
+  
+            <div className="box2"><img className="logo" src="/images/Logo2 - Copy.png"></img></div>
+
+            {/* <div className="Login"><h1>Oasis Savon</h1></div> */}
+
 
             <div className="Nav-bar">
+                <li className="nav-link" to='/Login'>Login/Register</li> 
                 <li className="nav-link" to='/Home'>Home</li>
                 <li className="nav-link" to='/about'>About</li>
-                <li className="nav-link" to='/shop'>Shop</li>    
+                <li className="nav-link" to='/shop'>Shop</li> 
+                  
             </div>
-                
-                
-            <div className="box2"><img className="logo" src="/images/Logo2.png"></img></div>
-         
 
-            <div className="Login"><li className="nav-link2" to='/Login'>Login/Register</li></div>
+            
         </div>
 
         )

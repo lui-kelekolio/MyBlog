5759b1dfe43122db454589c01aa68641d233a6e7
Lui Kelekolio <57427805+lui-kelekolio@users.noreply.github.com>
2020-08-19 23:58:15 +1200

    Create Footer.jsx

---
+++ client/components/Footer.jsx
@@ -0,0 +1,31 @@
+import react from 'react'
+
+class Footer extends React.Component {
+    constructor() {
+        super()
+
+        this.state = {}
+    }
+
+
+    render() {
+        return (
+            <>
+                <main>
+                    <div className="Contact"><p>Contact</p></div>
+
+
+                </main>
+
+            </>
+        )
+    }
+
+
+
+}
+
+
+
+
+export default Footer

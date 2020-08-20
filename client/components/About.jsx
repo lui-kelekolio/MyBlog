3d82265d29338c8cfec3da555353dfd1217528ba
Lui Kelekolio <57427805+lui-kelekolio@users.noreply.github.com>
2020-08-20 15:45:42 +1200

    Create About.jsx

---
+++ client/components/About.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+// import { HashRouter as Router, Route } from 'react-router-dom'
+
+
+    function About() {
+        return (
+
+                <main>
+                
+
+
+                </main>
+
+            
+        )
+    }
+
+
+
+
+
+
+
+
+export default About;

4797cc6b8e5066ae6e5eed96890f3409cc832bbd
Lui Kelekolio <57427805+lui-kelekolio@users.noreply.github.com>
2020-11-23 16:47:20 +1300

    Update Home.jsx

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-// import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 
 class Home extends React.Component {
@@ -14,29 +14,26 @@ class Home extends React.Component {
         return (
             <>
                 <main>
-                
-                                                                        
-                        <div className="hero-image-container">
-                            
-                            <img className="hero-image" src="/images/sidekix-media-g51F6-WYzyU-unsplash.jpg" />
-                                <div className="hero-page-headline">
-                                    <h1 className="hero-title">Welcome to Oasis Savon</h1>
-                                    <h3 className="hero-title2">testing testing 1 2 3</h3>
-                                </div>
-                                
-                        </div>
-
-                     
-
-
-                    <div className="blurb-container">
-                        <div className="blurb-headline"></div>
-                        <br />
-                        <div className="blurb-text"><p>This helps explain what we meant in the previous section when we said that racket provides require and the function-calling syntax. Libraries are not restricted to exporting values, such as functions; they can also define new syntactic forms.<br></br><br></br> In this sense, Racket isn’t exactly a language at all; it’s more of an idea for how to structure a language so that you can extend it or create entirely new languages.</p>
-
-                        </div>
+                    <div className="box1">
+                        <h1 className="title">WELCOME TO MY BLOG!!!</h1>
+                        <p className="sub-title">by Lui Kelekolio</p>
                     </div>
 
+                    <div className="box2">
+                            <div className="image-link1">
+                                <img className="home-image" src="/images/nice-view2.jpg" />
+                                <p className="text-link">A bit about me</p>
+                            </div>
+                            <div className="image-link1">
+                                <img className="home-image" src="/images/nice-view1.jpg" />
+                                <p className="text-link">Hobbies and Interests</p>
+                            </div>
+                            <div className="image-link1">
+                                <img className="home-image" src="/images/Big-Eyes-Post-782x1024.jpg" />
+                                <p className="text-link">Family</p>
+                            </div>
+                        
+                    </div>
 
                 </main>
 

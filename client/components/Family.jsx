24ac00e5bc04a82be39f7ca859b4f650f6e2f634
Lui Kelekolio <57427805+lui-kelekolio@users.noreply.github.com>
2020-11-23 18:10:17 +1300

    Create Family.jsx

---
+++ client/components/Family.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+// import { HashRouter as Router, Route } from 'react-router-dom'
+
+
+    function Family() {
+        return (
+
+                <main>
+                    <div className="About-container">
+                    <img className="about-hero-image" src="/images/jared-rice-PibraWHb4h8-unsplash.jpg" />
+                       
+                        <img className="about-image-logo" src="/images/Logo2 - Copy.png"></img>
+                        <div className="about-content-container">
+                        <div className="about-blurb-headline"><h1>Welcome to my new website!</h1></div>
+                        <div className="about-blurb-subtext">A result from the circle function is a picture value, which prints as an expression result in much the same way that numbers or strings print. The argument to circle determines the circle’s size in pixels.<br></br> As you might guess, there’s a rectangle function that takes two arguments instead of one:</div>
+                        </div>
+                    </div>
+
+
+                    <div className="who-we-are-container">
+                        <div className="wwa-blurb-headline"><h1>Who are we?</h1></div>
+                        <div className="wwa-blurb-subtext">A result from the circle function is a picture value, which prints as an expression result in much the same way that numbers or strings print. The argument to circle determines the circle’s size in pixels.<br></br> As you might guess, there’s a rectangle function that takes two arguments instead of one:</div>
+                    </div>
+                    
+
+                </main>
+
+            
+        )
+    }
+
+
+
+
+
+
+
+
+export default Family;

234507222de8888c8dd3b198bbfbdf604448f6ca
Lui Kelekolio <57427805+lui-kelekolio@users.noreply.github.com>
2020-11-23 17:41:57 +1300

    Update Home.jsx

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -19,6 +19,15 @@ class Home extends React.Component {
                         <p className="sub-title">by Lui Kelekolio</p>
                     </div>
 
+
+                    <div className="content">
+                        <p className="intro-para">Hey you, yeah you. What are you doing here? I wasn't expecting anyone. Well, since you're here, my name is Lui. Nice to meet you! 
+                            I am a graduate web designer and a huuuugee fan of video games. What you have stumbled upon is my attempt at a blog that I will keep up to date with 
+                            all the latest news going on with my life. Hope you enjoy! </p>
+
+                            <img className="profile-image" src="/images/nice-view2.jpg" />
+                    </div>
+
                     <div className="box2">
                             <div className="image-link1">
                                 <img className="home-image" src="/images/nice-view2.jpg" />
@@ -26,7 +35,7 @@ class Home extends React.Component {
                             </div>
                             <div className="image-link1">
                                 <img className="home-image" src="/images/nice-view1.jpg" />
-                                <p className="text-link">Hobbies and Interests</p>
+                                <p className="text-link">What am I doing with my life?</p>
                             </div>
                             <div className="image-link1">
                                 <img className="home-image" src="/images/Big-Eyes-Post-782x1024.jpg" />

a342d20d92b13afcfbab3537f1c9f47ee4ebd2dd
Lui Kelekolio <57427805+lui-kelekolio@users.noreply.github.com>
2020-12-01 02:25:45 +1300

    Update About.jsx

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -31,26 +31,28 @@ function About() {
                     <li>Favourite Movie: The Truman Show</li>
                 </ls>
             </div>
-            <p>My current skills and certificates include the following:</p>
-            <ls>
-                <li>NCEA levels 1,2 and 3 at Newlands College (2013-2015)</li>
-                <li>Work experience/Communications at Capital Training (2016)</li>
-                <li>Level 6 Web UI/UX Design at Yoobee/En Spiral Dev Academy (2018-2020)</li>
-                <li>Basic CPR Training at New World Thorndon (2017)</li>
-                <li>Fire Warden training at New World Churton Park (2020)</li>
-                <li>Forklift training at New World Churton Park (2020)</li>
-                <li>Duty Manager training at New World Churton Park (2020)</li>
 
-            </ls>
+            <div className="about-me-certs">
+                <p>My current skills and certificates include the following:</p>
+                <ls>
+                    <li>NCEA levels 1,2 and 3 at Newlands College (2013-2015)</li>
+                    <li>Work experience/Communications at Capital Training (2016)</li>
+                    <li>Level 6 Web UI/UX Design at Yoobee/En Spiral Dev Academy (2018-2020)</li>
+                    <li>Basic CPR Training at New World Thorndon (2017)</li>
+                    <li>Fire Warden training at New World Churton Park (2020)</li>
+                    <li>Forklift training at New World Churton Park (2020)</li>
+                    <li>Duty Manager training at New World Churton Park (2020)</li>
 
-            <p>My current work history:</p>
-            <ls>
-                <li>Cleaning Assistant at New World Thorndon (2016-2017)</li>
-                <li>Grocery Assistant at New World Thorndon (2017-2018)</li>
-                <li>Grocery Supervisor at New World Thorndon (2018-2020)</li>
-                <li>Duty Manager at Churton Park New World (Present)</li>
-            </ls>
+                </ls>
 
+                <p>My current work history:</p>
+                <ls>
+                    <li>Cleaning Assistant at New World Thorndon (2016-2017)</li>
+                    <li>Grocery Assistant at New World Thorndon (2017-2018)</li>
+                    <li>Grocery Supervisor at New World Thorndon (2018-2020)</li>
+                    <li>Duty Manager at Churton Park New World (Present)</li>
+                </ls>
+            </div>
 
 
         </main >

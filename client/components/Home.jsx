da18b38621ce1fa61bad41540c6efdb684603a90
Lui Kelekolio <57427805+lui-kelekolio@users.noreply.github.com>
2020-11-30 21:59:58 +1300

    Update Home.jsx

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -32,22 +32,27 @@ class Home extends React.Component {
 
                         <div className="about-me-container">
                             <div className="home-image-div">
-                                <Link to='/About'><img className="home-image" src=" /images/profile1.jpg" /></Link>
+                                <Link to='/About'><img className="about-image" src=" /images/profile1.jpg" /></Link>
                             </div>
-                            <div className="home-intro-div">
+                            <div className="about-intro-div">
                                 <p className="about-me-para">Hi! My name is Lui Kelekolio. I'm 22 years old and was both born and raised in  Wellington New Zealand. I am an aspiring front end developer with a diploma in level 6 UI/UX Web design. I've studied at places such as Yoobee's School of design and En Spiral Dev Academy so I have experience when it comes to maintaining a functionally, aesthetically pleasing website.</p>
                             </div>
                         </div>
 
 
-                        <div className="image-link2">
-                            <Link to='/Life'><img className="home-image2    " src="/images/received_332924380699747.jpeg" /></Link>
-                            <Link to='/Life'><p className="text-link2">What am I doing with my life?</p></Link>
+                        <div className="hobbies-container">
+                            <div className="hobbies-intro-div">
+                                <p className="hobbies-para">Outside of coding I very much enjoy sports, playing video games and spending time with friends and family. At the moment my favourite game would have to be Rocket League.<br></br> Add me! Would be cool to play some time!<br></br><br></br><a className="steam-link" href="https://steamcommunity.com/profiles/76561198179209627/">Mr.Loooooway</a></p>
+                            </div>
+                            <div className="hobbies-image-div">
+                                <Link to='/Life'><img className="hobbies-image" src="/images/received_332924380699747.jpeg" /></Link>
+                            </div>
+
                         </div>
-                        <div className="image-link3">
+                        {/* <div className="image-link3">
                             <Link to='/Family'><img className="home-image" src="/images/Big-Eyes-Post-782x1024.jpg" /></Link>
                             <Link to='/Family'><p className="text-link">Family</p></Link>
-                        </div>
+                        </div> */}
 
                     </div>
 

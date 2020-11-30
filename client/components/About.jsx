8468aed1eb4771a9937a5d6dab2cf2e5666cec3d
Lui Kelekolio <57427805+lui-kelekolio@users.noreply.github.com>
2020-12-01 01:55:50 +1300

    Update About.jsx

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -12,11 +12,24 @@ function About() {
                 <h1 className="whoami-title">Who am I?</h1>
             </div>
 
-            <div className="about-me-container2">
+            <div className="about-me-container1">
                 <h1 className="about-me-title">Hey! I'm Lui Kelekolio</h1>
-            </div>
-            <p>I'm 22 years old and was both born and raised in  Wellington, New Zealand. I am an aspiring front end developer with a passion for I.T. I've always had an interest in computers growing up. Being a gamer I've pretty much grown up using a computer so it felt natural for me to transition into coding. What I like about coding is that you really don't need to be a genius at maths or english or anything like that. The best skill to have in my opinion is the ability to problem solve. If you're good at that then I believe you'd be great at coding, so this is something that drives me to improve my technical skills. I was never the best at maths, same goes for english. But what I am good at is problem solving so with this I believe I am capable of reaching heights I never would have been able to dream of in my career. But for now I'm still only a graduate developer which means I have a lot of work to put in before I'm able to reach my goal. But with the support of friends and family behind me, I'm ready to take on the world of coding!</p>
 
+                <p className="about-me-content">I'm 22 years old and was both born and raised in  Wellington, New Zealand. I am an aspiring front end developer with a passion for I.T. I've always had an interest in computers growing up. Being a gamer I've pretty much grown up using a computer so it felt natural for me to transition into coding. What I like about coding is that you really don't need to be a genius at maths or english or anything like that. The best skill to have in my opinion is the ability to problem solve. If you're good at that then I believe you'd be great at coding, so this is something that drives me to improve my technical skills. I was never the best at maths, same goes for english. But what I am good at is problem solving so with this I believe I am capable of reaching heights I never would have been able to dream of in my career. But for now I'm still only a graduate developer which means I have a lot of work to put in before I'm able to reach my goal. But with the support of friends and family behind me, I'm ready to take on the world of coding!</p>
+            </div>
+            <div className="about-me-container2">
+                <h1 className="about-me-title2">Quick facts about myself</h1>
+                <ls className="about-me-content2">
+                    <li>Favourite Colour: Blue</li>
+                    <li>Favourite Food: Mum's Mac n Cheese</li>
+                    <li>Favourite Sport: Rugby</li>
+                    <li>Favourite Coding Language: CSS</li>
+                    <li>Favourite Video Game: Rocket League/Valorant</li>
+                    <li>Favourite Flavoured Ice-Cream: Cookies n Cream</li>
+                    <li>Favourite Fruit: Strawberries</li>
+                    <li>Favourite Movie: The Truman Show</li>
+                </ls>
+            </div>
             <p>My current skills and certificates include the following:</p>
             <ls>
                 <li>NCEA levels 1,2 and 3 at Newlands College (2013-2015)</li>

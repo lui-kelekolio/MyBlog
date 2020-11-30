1ff01404b8134d1575d7647b7891da6965f86681
Lui Kelekolio <57427805+lui-kelekolio@users.noreply.github.com>
2020-11-30 23:32:37 +1300

    Update About.jsx

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -3,32 +3,31 @@ import React from 'react';
 // import { HashRouter as Router, Route } from 'react-router-dom'
 
 
-    function About() {
-        return (
+function About() {
+    return (
 
-                <main>
-                    <div className="About-container">
-                    <img className="about-hero-image" src="/images/jared-rice-PibraWHb4h8-unsplash.jpg" />
-                       
-                        <img className="about-image-logo" src="/images/Logo2 - Copy.png"></img>
-                        <div className="about-content-container">
-                        <div className="about-blurb-headline"><h1>Welcome to my new website!</h1></div>
-                        <div className="about-blurb-subtext">A result from the circle function is a picture value, which prints as an expression result in much the same way that numbers or strings print. The argument to circle determines the circle’s size in pixels.<br></br> As you might guess, there’s a rectangle function that takes two arguments instead of one:</div>
-                        </div>
-                    </div>
+        <main>
+            <div className="who-am-i-container">
+                <img className="whoami-hero-image" src="/images/profile1.jpg" />
+                <h1 className="whoami-title">Who am I?</h1>
+            </div>
+
+            <div className="about-me-container2">
+                <h1 className="about-me-title">dsfsfvdsv</h1>
+            </div>
 
 
-                    <div className="who-we-are-container">
-                        <div className="wwa-blurb-headline"><h1>Who are we?</h1></div>
-                        <div className="wwa-blurb-subtext">A result from the circle function is a picture value, which prints as an expression result in much the same way that numbers or strings print. The argument to circle determines the circle’s size in pixels.<br></br> As you might guess, there’s a rectangle function that takes two arguments instead of one:</div>
-                    </div>
-                    
+            <div className="who-we-are-container">
+                <div className="wwa-blurb-headline"><h1>Who are we?</h1></div>
+                <div className="wwa-blurb-subtext">A result from the circle function is a picture value, which prints as an expression result in much the same way that numbers or strings print. The argument to circle determines the circle’s size in pixels.<br></br> As you might guess, there’s a rectangle function that takes two arguments instead of one:</div>
+            </div>
 
-                </main>
 
-            
-        )
-    }
+        </main >
+
+
+    )
+}
 
 
 

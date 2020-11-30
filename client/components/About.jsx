255892888fafddefdf4223ae0b668b46878c5cd6
Lui Kelekolio <57427805+lui-kelekolio@users.noreply.github.com>
2020-12-01 01:16:45 +1300

    Update About.jsx

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -14,15 +14,17 @@ function About() {
 
             <div className="about-me-container2">
                 <h1 className="about-me-title">Hey! I'm Lui Kelekolio</h1>
-                <p>I'm 22 years old and was both born and raised in  Wellington, New Zealand. I am an aspiring front end developer with a passion for I.T. I've always had an interest in computers since I was young. Being a gamer myself I've found </p>
             </div>
+            <p>I'm 22 years old and was both born and raised in  Wellington, New Zealand. I am an aspiring front end developer with a passion for I.T. I've always had an interest in computers growing up. Being a gamer I've pretty much grown up using a computer so it felt natural for me to transition into coding. What I like about coding is that you really don't need to be a genius at maths or english or anything like that. The best skill to have in my opinion is the ability to problem solve. If you're good at that then I believe you'd be great at coding, so this is something that drives me to improve my technical skills. I was never the best at maths, same goes for english. But what I am good at is problem solving so with this I believe I am capable of reaching heights I never would have been able to dream of in my career. But for now I'm still only a graduate developer which means I have a lot of work to put in before I'm able to reach my goal. But with the support of friends and family behind me, I'm ready to take on the world of coding!</p>
+
             <p>My current skills and certificates include the following:</p>
             <ls>
                 <li>NCEA levels 1,2 and 3 at Newlands College (2013-2015)</li>
                 <li>Work experience/Communications at Capital Training (2016)</li>
-                <li>Level 6 Web UI/UX Design (2018-2020)</li>
+                <li>Level 6 Web UI/UX Design at Yoobee/En Spiral Dev Academy (2018-2020)</li>
                 <li>Basic CPR Training at New World Thorndon (2017)</li>
                 <li>Fire Warden training at New World Churton Park (2020)</li>
+                <li>Forklift training at New World Churton Park (2020)</li>
                 <li>Duty Manager training at New World Churton Park (2020)</li>
 
             </ls>

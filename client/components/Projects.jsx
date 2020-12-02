4d59582ec791421d1a63423d3b24165159b751e7
Lui Kelekolio <57427805+lui-kelekolio@users.noreply.github.com>
2020-12-03 00:52:01 +1300

    Create Projects.jsx

---
+++ client/components/Projects.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+
+class Projects extends React.Component {
+    constructor() {
+        super()
+
+        this.state = {}
+    }
+
+
+    render() {
+        return (
+
+
+            <div className="Projects-container">
+                <div className="Projects-intro-title">
+                    <h1>HERE ARE A FEW OF MY PROJECTS:</h1>
+                </div>
+
+
+
+            </div>
+
+        )
+
+
+    }
+
+}
+
+export default Projects

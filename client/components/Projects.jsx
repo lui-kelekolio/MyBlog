f526b08103828a3482b110c194409377fd239291
Lui Kelekolio <57427805+lui-kelekolio@users.noreply.github.com>
2020-12-09 16:22:59 +1300

    Update Projects.jsx

--- client/components/Projects.jsx
+++ client/components/Projects.jsx
@@ -16,13 +16,13 @@ class Projects extends React.Component {
 
 
             <div className="Projects-container">
-                <div className="Projects-intro-title">
-                    <p className="projects-title">HERE ARE A FEW OF MY PROJECTS:</p>
-                </div>
+
 
 
                 <div className="Projects-links-container">
-
+                    <div className="Projects-intro-title">
+                        <p className="projects-title">HERE ARE A FEW OF MY PROJECTS:</p>
+                    </div>
                     <div className="projects-line1">
                         <div className="Fml-container">
                             <p className="projects-subtitle">Follow My Lead (En Spiral Dev Academy Final Group Project)</p>
@@ -31,32 +31,28 @@ class Projects extends React.Component {
                             </div>
                         </div>
 
+
                         <div className="Fml-container">
-                            <div className="Fml-container">
-                                <p className="projects-subtitle">Gaming Website (Personal Project)</p>
-                                <div className="fml-image-div">
-                                    <Link to='/Hobbies'><img className="fml-image" src="/images/gaming.jpg" /></Link>
-                                </div>
+                            <p className="projects-subtitle">Gaming Website (Personal Project)</p>
+                            <div className="fml-image-div">
+                                <Link to='/Hobbies'><img className="fml-image" src="/images/gaming.jpg" /></Link>
                             </div>
                         </div>
                     </div>
 
                     <div className="projects-line1">
                         <div className="Fml-container">
-                            <div className="Fml-container">
-                                <p className="projects-subtitle">Shopping Website (Udemy Final Project)</p>
-                                <div className="fml-image-div">
-                                    <Link to='/Hobbies'><img className="fml-image" src="/images/ice-dog.jpg" /></Link>
-                                </div>
+                            <p className="projects-subtitle">Shopping Website (Udemy Final Project)</p>
+                            <div className="fml-image-div">
+                                <Link to='/Hobbies'><img className="fml-image" src="/images/ice-dog.jpg" /></Link>
                             </div>
                         </div>
 
+
                         <div className="Fml-container">
-                            <div className="Fml-container">
-                                <p className="projects-subtitle">Minesweeper (En Spiral Dev Academy JS Project)</p>
-                                <div className="fml-image-div">
-                                    <Link to='/Hobbies'><img className="fml-image" src="/images/ice-dog.jpg" /></Link>
-                                </div>
+                            <p className="projects-subtitle">Minesweeper (En Spiral Dev Academy JS Project)</p>
+                            <div className="fml-image-div">
+                                <Link to='/Hobbies'><img className="fml-image" src="/images/ice-dog.jpg" /></Link>
                             </div>
                         </div>
                     </div>

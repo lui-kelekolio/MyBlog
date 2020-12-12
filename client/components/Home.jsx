2d7638cadc576e117fb15596257340dc8e0c49de
Lui Kelekolio <57427805+lui-kelekolio@users.noreply.github.com>
2020-12-12 19:22:29 +1300

    Update Home.jsx

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -29,13 +29,13 @@ class Home extends React.Component {
                         <img className="profile-image" src="/images/profile2.jpg" />
                     </div>
 
-                    <div>
+                    <div className="main-container">
 
                         <div className="about-me-container">
                             <div className="about-image-div">
                                 <Link to='/About'><img className="about-image" src=" /images/profile1.jpg" /></Link>
                             </div>
-                            <div className="about-intro-div">
+                            <div>
                                 <p className="about-me-para">Hi! My name is Lui Kelekolio. I'm 22 years old and was both born and raised in  Wellington New Zealand. I am an aspiring front end developer with a diploma in level 6 UI/UX Web design. I've studied at places such as Yoobee's School of design and En Spiral Dev Academy so I have experience when it comes to maintaining a functionally, aesthetically pleasing website.</p>
                             </div>
                         </div>
@@ -45,7 +45,7 @@ class Home extends React.Component {
                             <div className="hobbies-image-div">
                                 <Link to='/Hobbies'><img className="hobbies-image" src="/images/received_474066093286936.jpeg" /></Link>
                             </div>
-                            <div className="hobbies-intro-div">
+                            <div>
                                 <p className="hobbies-para">Outside of coding I very much enjoy sports, listening to music, playing video games and spending time with friends and family. At the moment my favourite game would have to be Rocket League.<br></br> Add me! Would be cool to play some time!<br></br><br></br><a className="steam-link" href="https://steamcommunity.com/profiles/76561198179209627/">Steam Profile click here!</a></p>
                             </div>
 
@@ -55,19 +55,19 @@ class Home extends React.Component {
                             <div className="plan-image-div">
                                 <Link to='/Plan'><img className="plan-image" src="/images/kahu1.jpg" /></Link>
                             </div>
-                            <div className="plan-intro-div">
+                            <div>
                                 <p className="plan-para">My goal is to become an efficient front end developer. I still have a lot to do to achieve this goal but I have never been more motivated to give this my all. I plan on further improving my knowledge and technical skills to help me achieve this goal.</p>
                             </div>
 
                         </div>
-                        <div className="projects-main-container">
-                            <div className="Projects-title-container">
-                                <h1 className="projects-title">HERE ARE A FEW OF MY PROJECTS:</h1>
-                            </div>
-                        </div>
-                        <Projects />
-                    </div>
 
+                    </div>
+                    <div className="projects-main-container">
+                        <div className="Projects-title-container">
+                            <h1 className="projects-title">HERE ARE A FEW OF MY PROJECTS:</h1>
+                        </div>
+                    </div>
+                    <Projects />
 
 
                 </main>

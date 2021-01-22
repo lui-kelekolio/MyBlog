bf1a2d3f9f76f7c1e25373cb9e207d5c45ec0dff
Lui Kelekolio <57427805+lui-kelekolio@users.noreply.github.com>
2021-01-22 16:18:56 +1300

    Update Projects.jsx

--- client/components/Projects.jsx
+++ client/components/Projects.jsx
@@ -21,45 +21,60 @@ class Projects extends React.Component {
 
                 <div className="Projects-links-container">
 
-                    <div className="projects-line1">
-                        <div className="Fml-container">
-                            <p className="projects-subtitle">Follow My Lead (En Spiral Dev Academy Final Group Project)</p>
+                    <div className="projects-flex-1">
+                        <div className="project-container">
+                            <div className="div-block-projects">.</div>
+                            <p className="projects-subtitle">Follow My Lead (EDA Final Group Project)</p>
                             <div className="fml-image-div">
-                                <Link to='/Hobbies'><img className="fml-image" src="/images/ice-dog.jpg" /></Link>
+                                <img className="project-photo" src="/images/ice-dog.jpg" />
+                                <div className="div-block-projects">.</div>
+
                             </div>
                         </div>
+                        <div className="div-block-projects-seperate1">.</div>
 
-
-                        <div className="Fml-container">
+                        <div className="project-container">
+                            <div className="div-block-projects">.</div>
                             <p className="projects-subtitle">Gaming App (Personal Project)</p>
-                            <div className="fml-image-div">
-                                <Link to='/Hobbies'><img className="fml-image" src="/images/gaming.jpg" /></Link>
+                            <div className="fml-image-div" >
+                                <img className="project-photo" src="/images/gaming.jpg" />
+                                <div className="div-block-projects">.</div>
+
+
                             </div>
                         </div>
+                        <div className="div-block-projects-seperate2">.</div>
                     </div>
 
-                    <div className="projects-line1">
-                        <div className="Fml-container">
-                            <p className="projects-subtitle">Shopping Website (Udemy Final Project)</p>
-                            <div className="fml-image-div">
-                                <Link to='/Hobbies'><img className="fml-image" src="/images/ice-dog.jpg" /></Link>
-                            </div>
-                        </div>
 
+                    <div className="project-container">
+                        <div className="div-block-projects">.</div>
+                        <p className="projects-subtitle">Shopping Website (Udemy Final Project)</p>
+                        <div className="fml-image-div">
+                            <img className="project-photo" src="/images/shopping.jpg" />
+                            <div className="div-block-projects">.</div>
 
-                        <div className="Fml-container">
-                            <p className="projects-subtitle">Minesweeper (En Spiral Dev Academy JS Project)</p>
-                            <div className="fml-image-div">
-                                <Link to='/Hobbies'><img className="fml-image" src="/images/ice-dog.jpg" /></Link>
-                            </div>
                         </div>
                     </div>
+                    <div className="div-block-projects-seperate1">.</div>
 
-                </div>
+                    <div className="project-container">
+                        <div className="div-block-projects">.</div>
+                        <p className="projects-subtitle">Minesweeper (En Spiral Dev Academy JS Project)</p>
+                        <div className="fml-image-div">
+                            <img className="project-photo" src="/images/minesweep.jpg" />
+                            <div className="div-block-projects">.</div>
+
+                        </div>
+                    </div>
+                    <div className="div-block-projects-seperate2">.</div>
+                </div >
+
+            </div >
 
 
 
-            </div>
+
 
         )
 

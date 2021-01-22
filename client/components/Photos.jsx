c09531b43c8de7c33f2e2a2e1d3972706df5f705
Lui Kelekolio <57427805+lui-kelekolio@users.noreply.github.com>
2021-01-22 16:18:54 +1300

    Create Photos.jsx

---
+++ client/components/Photos.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+// import { HashRouter as Router, Route } from 'react-router-dom'
+
+
+function Photos() {
+    return (
+
+        <main>
+            <div className="box1">
+                <h1 className="title">PHOTO GALLERY</h1>
+
+            </div>
+
+            <img className="album-photo" src="/images/album.png" />
+
+
+
+
+
+
+        </main >
+
+
+    )
+}
+
+
+
+
+
+
+
+
+export default Photos;

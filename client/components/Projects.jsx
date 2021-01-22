903fb7804e7a291ce02a08f67a75542e61d62817
Lui Kelekolio <57427805+lui-kelekolio@users.noreply.github.com>
2021-01-22 16:37:51 +1300

    Update Projects.jsx

--- client/components/Projects.jsx
+++ client/components/Projects.jsx
@@ -25,8 +25,9 @@ class Projects extends React.Component {
                         <div className="project-container">
                             <div className="div-block-projects">.</div>
                             <p className="projects-subtitle">Follow My Lead (EDA Final Group Project)</p>
-                            <div className="fml-image-div">
+                            <div className="project-info-container">
                                 <img className="project-photo" src="/images/ice-dog.jpg" />
+                                <p className="project-para">This is a website that me and five other developers made during our final week of EDA. We call it Follow My Lead. This web app gives you the opportunity to connect will fellow dog lovers in your area to meet and arrange dog walks with them.</p>
                                 <div className="div-block-projects">.</div>
 
                             </div>
@@ -36,8 +37,9 @@ class Projects extends React.Component {
                         <div className="project-container">
                             <div className="div-block-projects">.</div>
                             <p className="projects-subtitle">Gaming App (Personal Project)</p>
-                            <div className="fml-image-div" >
+                            <div className="project-info-container" >
                                 <img className="project-photo" src="/images/gaming.jpg" />
+                                <p className="project-para">This has been a personal project of my own that I decided to do while studying at EDA, the main purpose of this website is to promote my friend's gaming channel.</p>
                                 <div className="div-block-projects">.</div>
 
 
@@ -50,8 +52,9 @@ class Projects extends React.Component {
                     <div className="project-container">
                         <div className="div-block-projects">.</div>
                         <p className="projects-subtitle">Shopping Website (Udemy Final Project)</p>
-                        <div className="fml-image-div">
+                        <div className="project-info-container">
                             <img className="project-photo" src="/images/shopping.jpg" />
+                            <p className="project-para">This is a web app that I completed while doing my own personal study part time using the Udemy code course on Javascript. </p>
                             <div className="div-block-projects">.</div>
 
                         </div>
@@ -61,8 +64,9 @@ class Projects extends React.Component {
                     <div className="project-container">
                         <div className="div-block-projects">.</div>
                         <p className="projects-subtitle">Minesweeper (En Spiral Dev Academy JS Project)</p>
-                        <div className="fml-image-div">
+                        <div className="project-info-container">
                             <img className="project-photo" src="/images/minesweep.jpg" />
+                            <p className="project-para">This is a simple app that I made during my first week of EDA. A small simple game of minesweeper!</p>
                             <div className="div-block-projects">.</div>
 
                         </div>

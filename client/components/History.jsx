638e8e381a83169f6274f60b8055b09847a5bce8
Lui Kelekolio <57427805+lui-kelekolio@users.noreply.github.com>
2021-01-28 15:02:04 +1300

    Create History.jsx

---
+++ client/components/History.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+
+class History extends React.Component {
+    constructor() {
+        super()
+
+        this.state = {}
+    }
+
+
+    render() {
+        return (
+
+
+            <div className="Projects-container">
+
+                <p>
+                    history
+                </p>
+
+
+
+            </div >
+
+
+
+
+
+        )
+
+
+    }
+
+}
+
+export default History

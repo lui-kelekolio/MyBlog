ac90aaac927f919dd0c84aa506dcbee2da26027b
Lui Kelekolio <57427805+lui-kelekolio@users.noreply.github.com>
2021-01-29 17:27:19 +1300

    Update About.jsx

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -16,7 +16,7 @@ function About() {
             <div className="about-me-container1">
                 <img className="about-me-profile" src="/images/profile2.jpg" />
                 {/* <h1 className="about-me-title">Hey! I'm Lui Kelekolio</h1> */}
-                <p className="about-me-content">I'm a 23 year old Wellingtonian with a newly found passion for coding. Other major interests include gaming, listening to music, playing sports, hanging out with mates and spending time with the fam. I also enjoy the old beer or two on the weekends to celebrate making it through the week.  </p>
+                <p className="about-me-content">I'm a 23 year old Wellingtonian with a newly found passion for coding. Other major interests include gaming, listening to music, playing sports, hanging out with mates and spending time with the fam. I also enjoy the odd beer or two on the weekends to celebrate making it through the week.  </p>
             </div>
             <div className="about-me-container2">
 

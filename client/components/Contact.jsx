fb167e6db5152028e8b336747bcd8c8e84842f44
Lui Kelekolio <57427805+lui-kelekolio@users.noreply.github.com>
2021-02-01 16:30:06 +1300

    Update Contact.jsx

--- client/components/Contact.jsx
+++ client/components/Contact.jsx
@@ -19,10 +19,20 @@ class Contact extends React.Component {
                     <img className="Contact-hero-image" />
                     <h1 className="whoami-title">Contact</h1>
                 </div>
+                <div className="content">
+                    <p className="intro-para">Feel free to contact me through these details if you have anything you'd like to ask, any advice you'd like to give or if you just feel like having a chat. Always keen to meet new people so if you'd like to arrange a get together just let me know. Thank you!</p>
 
+                    <img className="profile-image" src="/images/profile1.jpg" />
+
+                </div>
 
                 <div className="Contact-container">
-
+                    <img className="Contact-logo" src="/images/fb.webp" />
+                    <img className="Contact-logo" src="/images/insta.webp" />
+                    <img className="Contact-logo" src="/images/steam.png" />
+                    <img className="Contact-logo" src="/images/github.png" />
+                    <img className="Contact-logo" src="/images/linkedin.png" />
+                    <img className="Contact-logo" src="/images/gmail.jpg" />
 
                 </div >
             </div>

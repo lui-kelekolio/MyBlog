8d6e6d90901160495ee6e25de694950423e8ebdd
Lui Kelekolio <57427805+lui-kelekolio@users.noreply.github.com>
2021-03-02 17:57:05 +1300

    Update Contact.jsx

--- client/components/Contact.jsx
+++ client/components/Contact.jsx
@@ -15,10 +15,11 @@ class Contact extends React.Component {
         return (
 
             <div>
-                <div className="who-am-i-container">
-                    <Nav />
-                    <img className="Contact-hero-image" />
-                    <h1 className="whoami-title">Contact</h1>
+                <Nav />
+                <div className="Home-Hero-div">
+                    <h1 className="Hero-title">Contact</h1>
+                    <img className="Nav-hero-image" />
+
                 </div>
                 <div className="content">
                     <p className="intro-para">Feel free to contact me through these details if you have anything you'd like to ask, any advice you'd like to give or if you just feel like having a chat. Always keen to meet new people so if you'd like to arrange a get together just let me know. Thank you!</p>

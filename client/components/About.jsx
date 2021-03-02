1f9a8c0683b1ac1365af6fd757a4f73f1241e24c
Lui Kelekolio <57427805+lui-kelekolio@users.noreply.github.com>
2021-03-02 17:57:01 +1300

    Update About.jsx

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -8,11 +8,15 @@ function About() {
     return (
 
         <main>
-
-            <div className="who-am-i-container">
-                < Nav />
-                <img className="whoami-hero-image" />
-                <h1 className="whoami-title">About me</h1>
+            < Nav />
+            <div className="Home-Hero-div">
+                <h1 className="Hero-title">About Me </h1>
+                <div className="Hero-image-container">
+                    <img className="Hero-image" src="/images/kahu1.jpg" />
+                </div>
+                <div className="Toggle-background">
+                    <span className="Toggle-background-color"></span>
+                </div>
             </div>
 
             <div className="about-me-container1">
@@ -40,7 +44,7 @@ function About() {
 
                     {/* <h1 className="about-me-title">Hey! I'm Lui Kelekolio</h1> */}
                     <p className="about-me-content">My dream is to become an outstanding front end developer, with this in mind I've dedicated most of my time to improving my technical skills. I've still got a long way to go before I can say that I've achieved this dream but I'm looking forward to the journey ahead. </p>
-                    <img className="about-me-profile1" src="/images/steam.png" />
+                    {/* <img className="about-me-profile1" src="/images/steam.png" /> */}
                 </div>
             </div>
 
